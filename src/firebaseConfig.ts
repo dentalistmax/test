5c87d153a3048795863632c5dae2e5320b5c1859
AlyEl-deen <alyeldeenayoub@gmail.com>
2026-02-13 19:09:27 +0200

    Reactivate ( apiKey - projectId ) and commenting ( import { getAnalytics } from "firebase/analytics"; - const analytics = getAnalytics(app);  ) to fix the forms error.

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,16 +8,16 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- // apiKey: "AIzaSyCcDV8lZW1sjNzvewosmnCElOD_kb5VaGI",
+  apiKey: "AIzaSyCcDV8lZW1sjNzvewosmnCElOD_kb5VaGI",
   authDomain: "DentlistMax-web.firebaseapp.com",
   projectId: "DentlistMax-web",
   storageBucket: "DentlistMax-web.firebasestorage.app",
   messagingSenderId: "554374262613",
- // appId: "1:554374262613:web:e2321b3e6439bfc2e609dc",
+  appId: "1:554374262613:web:e2321b3e6439bfc2e609dc",
  // measurementId: "G-WR27S5HMNN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const db = getFirestore(app);
